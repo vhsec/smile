8718004ede3479de12b4afbcc18ba60e23a0b53d
Unix <root@localhost>
2022-09-18 13:21:14 +0700

    up

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
